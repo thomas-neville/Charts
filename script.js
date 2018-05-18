b3601ca9ed1d9c3c51b0b208214bc29777d162e2
Thomas Neville <tnev@uw.edu>
2018-05-17 19:57:58 -0700

    change pie to bar chart

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("let's build some charts!");
 
  // Load the Visualization API and the corechart package. 
  // Only has to happen once
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['corechart','bar']});
 
 // For second chart:
 // 1. Set a callback for it (setOnLoadCallback)
@@ -19,29 +19,26 @@ google.charts.setOnLoadCallback(drawChart2);
 // draws it.
 function drawChart1() {
 
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-    ['Cheese', 1],
-    ['Taco', 3],
-    ['Veggie Thai', 2],
-    ['Hawaiian', 3],
-    ['Pepperoni', 2]
-  ]);
+  var data = google.visualization.arrayToDataTable([
+          ['Toppings', 'Slices'],
+          ['Cheese', 1],
+          ['Taco', 3],
+          ['Veggie Thai', 2],
+          ['Hawaiian', 3],
+          ['Pepperoni', 2]
+        ]);
 
-  // Set chart options
-  var options = {'title':'What Kinds of Pizza I Ate Last Night',
-                 'width':500,
-                 'height':500,
-                 'legend':'left',
-                 'is3D':true,                
-                };
+        var options = {
+          chart: {
+            title: 'Pizza Eaten by Slices',
+          },
+          bars: 'horizontal' // Required for Material Bar Charts.
+          
+        };
 
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
-  chart.draw(data, options);
+        var chart = new google.charts.Bar(document.getElementById('chart_div1'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
 }
 
 function drawChart2() {
